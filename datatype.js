7131f3955a1f0ce351bdcee8f1ee4e8141d09c35
rupa-shetty <shettyrupa751@gmail.com>
2021-03-24 20:58:10 +0530

    js file

---
+++ datatype.js
@@ -0,0 +1,17 @@
+var emp_No=101;
+var emp_Name='Raju';
+var is_Avail=true;
+var emp_Sal;
+
+var size_Shirt=['30','40','42'];
+var employee={
+    name='ramesh', age='25',loc='goa'
+};
+
+console .log(typeof emp_No);
+console .log(typeof emp_Name);
+console .log(typeof is_Avail);
+console .log(typeof emp_Sal);
+
+console .log(typeof size_Shirt);
+console .log(typeof employee);
