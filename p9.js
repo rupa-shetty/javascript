e1612091750360fd8c4d3f0113cdc8df71fb2535
rupa-shetty <shettyrupa751@gmail.com>
2021-03-25 17:07:59 +0530

    Mulp of 6

---
+++ p9.js
@@ -0,0 +1,16 @@
+var num=6;
+var i=1;
+while(i<=10)
+{
+    var result=num*i;
+    console.log(`${num} * ${i} = ${result}`);
+    i++;
+}
+
+/*for(i = 1; i <= 10; i++) 
+{
+    var result=num*i;
+
+    // display the result
+    console.log(`${num} * ${i} = ${result}`);
+}*/
