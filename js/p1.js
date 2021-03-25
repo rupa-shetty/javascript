1860229f11ad4408d6e93784409cb4e90d3a7740
rupa-shetty <shettyrupa751@gmail.com>
2021-03-25 12:01:48 +0530

    Div7

--- js/p1.js
+++ js/p1.js
@@ -1,1 +1,16 @@
+function div7(x)
+{
+    if (x%7==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
+console.log(div7(14));
+console.log(div7(20));
+console.log(div7(28));
+console.log(div7(70));
