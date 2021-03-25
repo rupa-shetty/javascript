37408cf124aa0ccca4bf25b649081692eda5504e
rupa-shetty <shettyrupa751@gmail.com>
2021-03-25 17:01:37 +0530

    Mul of 4

---
+++ p8.js
@@ -0,0 +1,15 @@
+var num;
+/*while(x<=10)
+{
+    console.log(`${x} * ${i} = ${result}`);
+    x++;
+}*/
+
+for(i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * num;
+
+    // display the result
+    console.log(`${num} * ${i} = ${result}`);
+}
