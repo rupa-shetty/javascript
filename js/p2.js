260c209a5f887244e82ee5035da294628bf623d2
rupa-shetty <shettyrupa751@gmail.com>
2021-03-25 12:04:31 +0530

    mul3

---
+++ js/p2.js
@@ -0,0 +1,16 @@
+function mul3(x)
+{
+    if (x%3==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+console.log(mul3(15));
+console.log(mul3(20));
+console.log(mul3(18));
+console.log(mul3(40));
