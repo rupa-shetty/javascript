7e5c2bc5fdda1402d0fcf0c52f603ea1c4d29d1c
rupa-shetty <shettyrupa751@gmail.com>
2021-03-25 17:04:59 +0530

    Mulp of 4

--- p8.js
+++ p8.js
@@ -1,14 +1,13 @@
-var num;
+var num=4;
 /*while(x<=10)
 {
     console.log(`${x} * ${i} = ${result}`);
     x++;
 }*/
 
-for(i = 1; i <= 10; i++) {
-
-    // multiply i with number
-    const result = i * num;
+for(i = 1; i <= 10; i++) 
+{
+    var result=num*i;
 
     // display the result
     console.log(`${num} * ${i} = ${result}`);
