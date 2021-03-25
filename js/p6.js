2b38288d4db18fa1533a9ffb1af6f3d40f960227
rupa-shetty <shettyrupa751@gmail.com>
2021-03-25 12:13:06 +0530

    Even/Odd

---
+++ js/p6.js
@@ -0,0 +1,16 @@
+function even1(x)
+{
+    if (x%2==0)
+    {
+        return evennumber;
+    }
+    else
+    {
+        return oddnumber;
+    }
+}
+
+console.log(even1(15));
+console.log(even1(20));
+console.log(even1(18));
+console.log(even1(40));
